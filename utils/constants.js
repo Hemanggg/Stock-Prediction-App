ff997d3619522732639d94d8f114361f9046deb4
Hemanggg <142934998+Hemanggg@users.noreply.github.com>
2023-08-23 00:55:20 +0530

    Add files via upload

---
+++ utils/constants.js
@@ -0,0 +1,7 @@
+import { PublicKey } from "@solana/web3.js"
+
+export const RPC_ENDPOINT  = "https://orbital-yolo-diagram.solana-devnet.discover.quiknode.pro/e5e1075092052e91da50469e6f5a2086524054ca/"
+
+export const PROGRAM_ID = new PublicKey("4pL3ekggbcsNcmKP2NECNfcKr5zVv2h9Gju98C38mwXC");
+
+export const MINIMUM_REMAINING_TIME_UNTIL_EXPIRY = 120;
